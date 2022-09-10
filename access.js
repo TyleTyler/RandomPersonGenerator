082aea7e38661bc78c2bc57bbbc19b8a25bc6ea7
TylenolSniffinTyler <willythewhale8@gmail.com>
2022-09-10 15:00:09 -0400

    Update (09/10/22)

--- access.js
+++ access.js
@@ -1,13 +1,40 @@
+import {colors, music} from "./map.js"
+let skin = document.getElementById('pfp')
+let refresh = document.getElementById('refreshBut')
+let city = document.getElementById('city')
 let name = document.getElementById('nameEl')
+let age = document.getElementById('ageEl')
+let color = document.getElementById("color")
+let genre = document.getElementById('genre')
 let ourRequest = new XMLHttpRequest();
 let pH = document.getElementById('placeHolder')
-ourRequest.open('GET','https://filltext.com/?rows=10&fname={firstName}&lname={lastName}&pretty=true')
+ourRequest.open('GET','http://www.filltext.com/?rows=1&name={firstName}~{lastName}&age={numberRange|10,60}&location={addressObject}&favoriteColor={number|9}&favoriteGenre={number|10}&skinColor={numberRange|1,20}&pretty=true')
 ourRequest.onload = function(){
-    let ourData = JSON.parse(ourRequest.responseText)
-    name.innerText += ourData[0].fname
+    renderPerson(ourRequest)
 }
 
 ourRequest.send();
 
+function renderPerson(request){
+    let ourData = JSON.parse(request.responseText)
+    name.innerText += " " + ourData[0].name
+    age.innerText += " " + ourData[0].skinColor
+    color.innerText += " " + colors.get(ourData[0].favoriteColor)
+    genre.innerText += " " + music.get(ourData[0].favoriteGenre)
+    city.innerText += ` ${ourData[0].location.streetAddress}, ${ourData[0].location.city}, ${ourData[0].location.state}`
+    skin.src = `Skintones/Skintones[${ourData[0].skinColor}].png`
+}
 
-
+refresh.addEventListener('click',function(){
+    let tempReq = new XMLHttpRequest
+    tempReq.open('GET','http://www.filltext.com/?rows=1&name={firstName}~{lastName}&age={numberRange|10,60}&location={addressObject}&favoriteColor={number|9}&favoriteGenre={number|10}&skinColor={numberRange|1,20}&pretty=true')
+    tempReq.onload = function(){
+        renderPerson(tempReq)
+    }
+    name.innerText = "Name: "
+    age.innerText = "Age: "
+    color.innerText = "Favorite Color: "
+    genre.innerText = "Favorite Music Genre: "
+    city.innerText = "Location: "
+    tempReq.send();
+})
