78ea5a8093b4c9e0d4efda48830c6b824374d2a5
Wilson <willythewhale8@gmail.com>
2022-09-29 16:59:42 -0400

    test

---
+++ map.js
@@ -0,0 +1,28 @@
+
+
+const music = new Map()
+const colors = new Map()
+
+colors.set(0,"White")
+colors.set(1,"Red")
+colors.set(2,"Blue")
+colors.set(3,"Green")
+colors.set(4,"Yellow")
+colors.set(5,"Orange")
+colors.set(6, "Purple")
+colors.set(7, "Pink")
+colors.set(8, "Brown")
+colors.set(9, "Black")
+
+music.set(0, "Punk")
+music.set(1,"Country")
+music.set(2, "Techno")
+music.set(3, "Hip-Hop")
+music.set(4, "Pop")
+music.set(5,"Rock")
+music.set(6,"Rap")
+music.set(7, "Jazz")
+music.set(8, "Folk")
+music.set(9,"R&B")
+music.set(10, "Reggae")
+export{colors, music}
