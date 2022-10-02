23e0ba02f4c5a43ce0d7231f808d77a47ebdb322
Wilson <willythewhale8@gmail.com>
2022-10-02 11:37:24 -0400

    changes

--- access.js
+++ access.js
@@ -1,5 +1,5 @@
-import {colors, music} from "./map.js"
 
+const mergeImages = require('merge-images-v2')
 
 let skin = document.getElementById('pfp')
 let refresh = document.getElementById('refreshBut')
@@ -12,6 +12,12 @@ let ourRequest = new XMLHttpRequest();
 let pH = document.getElementById('placeHolder')
 
 
+let request = async () =>{
+
+    let req = await fetch('http://www.filltext.com/?rows=1&name={firstName}~{lastName}&age={numberRange|10,60}&location={addressObject}&favoriteColor={number|9}&favoriteGenre={number|10}&skinColor={numberRange|1,20}&pretty=true')
+    let data = await req.json()
+    return data;
+}
 refresh.addEventListener('click',function(){
     request().then(ourData =>{
         name.innerText = "Name: "
@@ -28,12 +34,6 @@ refresh.addEventListener('click',function(){
 })
 
 
-let request = async () =>{
-
-    let req = await fetch('http://www.filltext.com/?rows=1&name={firstName}~{lastName}&age={numberRange|10,60}&location={addressObject}&favoriteColor={number|9}&favoriteGenre={number|10}&skinColor={numberRange|1,20}&pretty=true')
-    let data = await req.json()
-    return data;
-}
 
 request().then(ourData =>{
     name.innerText += " " + ourData[0].name
@@ -41,4 +41,36 @@ request().then(ourData =>{
     color.innerHTML += "<br>" + colors.get(ourData[0].favoriteColor)
     genre.innerText += " " + music.get(ourData[0].favoriteGenre)
     city.innerText += ` ${ourData[0].location.streetAddress}, ${ourData[0].location.city}, ${ourData[0].location.state}`
+    mergeImages([`./Skintones/Skintones[${ourData[0].skinColor}].png`])
+    .then(b64 => skin.src = b64);
 })
+
+
+
+
+
+const music = new Map()
+const colors = new Map()
+
+colors.set(0,"White")
+colors.set(1,"Red")
+colors.set(2,"Blue")
+colors.set(3,"Green")
+colors.set(4,"Yellow")
+colors.set(5,"Orange")
+colors.set(6, "Purple")
+colors.set(7, "Pink")
+colors.set(8, "Brown")
+colors.set(9, "Black")
+
+music.set(0, "Punk")
+music.set(1,"Country")
+music.set(2, "Techno")
+music.set(3, "Hip-Hop")
+music.set(4, "Pop")
+music.set(5,"Rock")
+music.set(6,"Rap")
+music.set(7, "Jazz")
+music.set(8, "Folk")
+music.set(9,"R&B")
+music.set(10, "Reggae")
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.listen(3000)
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+
+
 app.get('/', (req, res) =>{
-    res.sendFile('./main.html', {root: __dirname })
+    console.log(req +  " has been made")
+    res.render('main')
 })
