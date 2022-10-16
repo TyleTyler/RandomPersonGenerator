e2b23b7f8733a6bd9022c28c0ac35d4659606d05
TylenolSniffinTyler <willythewhale8@gmail.com>
2022-10-16 16:05:34 -0400

    changees

--- s3.js
+++ s3.js
@@ -1,16 +1,41 @@
-const S3 = require('aws-sdk/clients/s3')
+const AWS = require('aws-sdk')
+const fs = require('fs')
+
+
 const bucketName ="randomgenpfp"
 const region = "us-east-1"
-const accessKeyId ="AKIA475CVX7EHV3JN6FG"
-const secretAccessKey ="60PpF1b327f4xQQZ8t/xpm8HeuTzzE"
-const S3 = new S3({
+const accessKeyId ="AKIA475CVX7ECTXYOYNV"
+const secretAccessKey ="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"
+const s3 = new AWS.S3({
     region,
     accessKeyId,
     secretAccessKey
 })
 
 //uploads a file to s3
+function uploadPfp(filePath, personName){
+    const fileStream = fs.createReadStream(filePath)
+
+    const uplaodParams = {
+        Bucket : bucketName,
+        Body : fileStream,
+        Key : personName
+    }
+
+    return s3.putObject(uplaodParams).promise()
+}
 
 
+exports.uploadPfp = uploadPfp
 
 //downloads a file from s3
+
+function getFile(fileKey){
+    downloadParams = {
+        Key : fileKey,
+        Bucket : bucketName
+    }
+    return s3.getObject(downloadParams).createReadStream()
+}
+
+exports.getFile = getFile 
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express();
 const genPfp = require('./access.js')
 const mongoose = require('mongoose')
 const Profile = require('./modles/profile');
-const { render } = require('ejs');
-
+const  tools = require('./s3')
+const { uploadPfp, getFile } = require('./s3')
+const fs = require('fs')
 
 const dbUri = 'mongodb+srv://wilsonr:test1234@cluster0.rrqn4vx.mongodb.net/SavedProfiles?retryWrites=true&w=majority'
 mongoose.connect(dbUri).then(res => {
@@ -40,28 +41,35 @@ app.get('/', (req, res) =>{
     })
 })
 
+
 app.get('/create-profile', (req, res) =>{
     res.render('create', {
         Header: "Create  a Profile"
     })
 })
+
+
 app.get('/saved-profiles', (req, res) =>{
     Profile.find().sort({createdAt  : -1})
     .then(result =>{
-        res.render('saved', { Header: "Saved-Profiles", list : result})
+        res.render('saved', { Header: "Saved-Profiles", list : result, pfp : tools})
     })
     .catch(err =>{
         console.log(err)
     })
 })
+
+
 app.post("/saved-profiles", (req, res) =>{
     Profile.find().then(length =>{
-        if(length.length >= 9){
-            console.log("Too many profiles")
-            res.redirect("/saved-profiles")
-            return
-        }
-        const profile = new Profile(req.body)
+    if(length.length >= 9){
+        console.log("Too many profiles")
+        res.redirect("/saved-profiles")
+        return
+    }
+    
+    const profile = new Profile(req.body)
+    uploadPfp("./public/pfpComponents/pfp.png", req.body.name + "'s Profile")
     profile.save()
     .then(pfp =>{
         res.redirect("/saved-profiles")
@@ -71,12 +79,22 @@ app.post("/saved-profiles", (req, res) =>{
     })    
 })
 
+
 app.get('/saved-profiles/:id', (req ,res) =>{
     const id = req.params.id
+    
     Profile.findById(id)
     .then(result =>{
-        res.render('savedprof', { Header : `${result.name}'s Profile`, pfp : result})
+        console.log(result.name)
+        getFile(result.name + "'s Profile").pipe(res)
+        // res.render('savedprof', { Header : `${result.name}'s Profile`, pfp : result})
     })
+})
+
+
+app.get('/test', (req, res) =>{
+    const readStream = getFile("Kelly's Profile")
+    readStream.pipe(res)
 })
 
 app.use((req, res) => {
